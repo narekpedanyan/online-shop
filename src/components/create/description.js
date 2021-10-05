f601a21ccfd64ef318e26ec57864134ca8f1d1eb
aslanyan66 <aslanyanazat66@gmail.com>
2021-10-05 16:34:51 +0400

    work in description component

--- src/components/create/description.js
+++ src/components/create/description.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React from "react";
+import { descriptionData } from "../../constants/carInfo";
 
-const Description = ({handleFieldChange}) => {
+const Description = ({ handleFieldChange }) => {
+  const { leftBlockData, rightBlockData } = descriptionData;
 
   return (
-    <div>
-      <input name="mark" type="text" onChange={handleFieldChange}/>
+    <div className="description">
+      {/* <input name="mark" type="text" onChange={handleFieldChange} /> */}
+      <div className="container">
+        <div className="description-content">
+          <div className="description-content-left">
+            {leftBlockData.map((inputName) => {
+              return (
+                <div className="description-content-left_item">
+                  <label>{inputName}</label>
+
+                  <input
+                    className="description-input"
+                    name={inputName}
+                    type={
+                      inputName === "year" ||
+                      inputName === "engine" ||
+                      inputName === "mileage"
+                        ? "number"
+                        : "text"
+                    }
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div className="description-content-right">
+            <div>dabbdbabda</div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Description;
--- src/containers/create/index.js
+++ src/containers/create/index.js
@@ -1,82 +1,83 @@
-import React, {useState} from 'react';
-import Description from '../../components/create/description';
-import Additional from '../../components/create/additional';
+import React, { useState } from "react";
+import Description from "../../components/create/description";
+import Additional from "../../components/create/additional";
 import Resources from "../../components/create/resources";
-import Publish from '../../components/create/publish';
-import './index.scss';
-import {carInfoModel} from "../../constants/carInfo";
-
+import Publish from "../../components/create/publish";
+import "./index.scss";
+import { carInfoModel } from "../../constants/carInfo";
 const steps = {
   description: {
-    key: 'description',
-    label: 'Description',
-    route: Description
+    key: "description",
+    label: "Description",
+    route: Description,
   },
   additional: {
-    key: 'additional',
-    label: 'Additional',
-    route: Additional
+    key: "additional",
+    label: "Additional",
+    route: Additional,
   },
   resources: {
-    key: 'resources',
-    label: 'Resources',
-    route: Resources
+    key: "resources",
+    label: "Resources",
+    route: Resources,
   },
   publish: {
-    key: 'publish',
-    label: 'Publish',
-    route: Publish
-  }
+    key: "publish",
+    label: "Publish",
+    route: Publish,
+  },
 };
 
 const Create = () => {
-  const [step, setStep] = useState(steps['description']);
-  const [productInfo, setProductInfo] = useState(carInfoModel)
-  const {route} = step;
+  const [step, setStep] = useState(steps["description"]);
+  const [productInfo, setProductInfo] = useState(carInfoModel);
+  const { route } = step;
 
   const handleFieldChange = (e) => {
-    const {name, value} = e.target
+    const { name, value } = e.target;
     setProductInfo((prev) => {
       return {
         ...prev,
-        [name]: value
-      }
-    })
-  }
+        [name]: value,
+      };
+    });
+  };
 
   const changeStep = (routeData) => {
     setStep(routeData);
   };
+  console.log(step.key);
 
   return (
     <section className="crate-screen">
       <div className="crate-content">
         <div className="steps-control">
           <div className="container">
-            {
-              Object.keys(steps).map((item) => {
-                const {label} = steps[item];
+            <div className="steps-control-list">
+              {Object.keys(steps).map((item) => {
+                const { label } = steps[item];
                 return (
                   <button
                     onClick={() => changeStep(steps[item])}
                     type="button"
                     key={item}
+                    className={
+                      step.key === label.toLowerCase() ? "btn-active" : null
+                    }
                   >
                     {label}
                   </button>
-                )
-              })
-            }
+                );
+              })}
+            </div>
           </div>
         </div>
         <div>
-          <div className="container">
-            {route({handleFieldChange})}
-          </div>
+          <div>{route({ handleFieldChange })}</div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Create;

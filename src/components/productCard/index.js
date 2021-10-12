dfe9b7d6752c76e59b30cda6455ed4154220df57
Artur Jomardyan <jomardyan19@gmail.com>
2021-10-12 17:29:09 +0400

    Home page change

--- src/components/productCard/index.js
+++ src/components/productCard/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './index.scss';
-import inchVorMIBan from '../../assets/media/car.png';
 
 const ProductCard = ({data}) => {
-    const {mark,
-      model,
-      price,
-      mileage,
-      fuel,
-      year,
-      engine
+    const {
+        mark,
+        model,
+        price,
+        mileage,
+        fuel,
+        year,
+        engine,
+        isSaleImmediat,
+        img,
+
     } = data;
     return (
         <div className="product-card">
             <div className="photoBox">
-              <img src={inchVorMIBan} alt="" />
+                <img src={img} alt=""/>
             </div>
-            <h4 className="carModelText">{mark} {model}  {engine}</h4>
-            <p className="carPrice">{price}</p>
+            <h4 className="carModelText">{mark} {model} {engine}</h4>
+            <div className="praceAndImmediate">
+                <p className="carPrice">{price}</p>
+                {isSaleImmediat && <div className="immediat">Շտապ</div>}
+            </div>
+
             <div className="carCardFooter">
-              <p className="footerText">{mileage}</p>
-              <p className="footerText">{fuel}</p>
-              <p className="footerText">{year}</p>
+                <p className="footerText">{mileage}</p>
+                <p className="footerText">{fuel}</p>
+                <p className="footerText">{year}</p>
             </div>
         </div>
     )
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,33 +2,36 @@ import React from 'react';
 import {useSelector} from 'react-redux';
 import './index.scss';
 import ProductCard from "../../components/productCard";
+import banner from '../../assets/media/banner.png';
+import vector from '../../assets/media/Vector.png';
+
 
 const Home = () => {
     const productsData = useSelector(state => state.products);
     const {data} = productsData;
-    const immediateData = data.filter((item) => item.isimmediate)
+    const top = data.filter((item) => item.topCount > 100)
+    const immediateData = data.filter((item) => item.isSaleImmediat)
     const newData = data.filter((item) => item.createDate - 24 < 0);
     return (
         <React.Fragment>
-            <section className="filters">
+            <section className="products">
                 <div className="container">
-                    <div>
-                        Filters
-                    </div>
+                    <h1 style={{border:"2px solid black",height:"100px"}}>Filter</h1>
                 </div>
             </section>
             <section className="products">
                 <div className="container">
+                    <h1 style={{margin: "56px 0"}}>Թոփ հայտարարություններ</h1>
                     <div className="products-grid">
                         {
-                            data.map((item, index) => (<ProductCard data={item} key={index}/>))
+                            top.map((item, index) => (<ProductCard data={item} key={index}/>))
                         }
                     </div>
                 </div>
             </section>
             <section className="immediateProducts">
                 <div className="container">
-                    <h1 style={{marginBottom: "56px"}}>Շտապ հայտարարություններ</h1>
+                    <h1 style={{margin: "56px 0"}}>Շտապ հայտարարություններ</h1>
                     <div className="products-grid">
                         {
                             immediateData.map((item, index) => (<ProductCard data={item} key={index}/>))
@@ -38,16 +41,29 @@ const Home = () => {
             </section>
             <div className="container">
                 <section className="BannerSection">
-                    <h1 className="BannerText">Banner</h1>
+                    <img src={banner} alt=""/>
                 </section>
             </div>
             <section className="NewProducts">
                 <div className="container">
-                    <h1 style={{marginBottom: "56px"}}>Նոր հայտարարություններ</h1>
+                    <h1 style={{margin: "56px 0"}}>Նոր հայտարարություններ</h1>
                     <div className="products-grid">
                         {
                             newData.map((item, index) => (<ProductCard data={item} key={index}/>))
                         }
+                    </div>
+                </div>
+            </section>
+            <section className="homeFooter">
+                <div className="container">
+                    <div className="foooterLeftSaid">
+                        <div className="footerLogo">
+                            <img src={vector} alt=""/>
+                            <a href="/">2021 Deo.am</a>
+                        </div>
+                        <a href="/">Հետադարձ կապ</a>
+                        <a href="/">Գովազդ</a>
+                        <a href="/">Օգտագործման պայմաններ</a>
                     </div>
                 </div>
             </section>

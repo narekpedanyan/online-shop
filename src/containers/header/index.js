e918c7e4e7baca681fd69df126a78925bed6dfb5
Edmon <edmongrogoryan.04@gmail.com>
2021-10-12 20:04:56 +0400

    h

--- src/containers/header/index.js
+++ src/containers/header/index.js
@@ -1,13 +1,99 @@
 import React from 'react';
 import './index.scss';
+import car1 from "./img/car 1.jpg";
+import car2 from "./img/car2.png";
+import car3 from "./img/car 3.png";
+import car4 from "./img/car 4.png";
+import car5 from "./img/car 5.png";
+import car6 from "./img/car 6.png";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container">
-        Header
+
+      <div className="Header">
+          <div id='header'>
+                <span className='blue'>
+                    <svg width="31" height="15" viewBox="0 0 33 17" fill="none" xmlns="http://www.w3.org/2000/svg"
+                         className='carSvg'>
+                    <rect width="33" height="17" fill="#E5E5E5"/>
+                    <g id="Home Page / Open" clip-path="url(#clip0)">
+                        <rect width="1440" height="3340" transform="translate(-127 -30)" fill="white"/>
+                        <g id="Header">
+                            <rect id="Rectangle 5" x="-127" y="-30" width="1440" height="90" fill="white"/>
+                            <g id="Frame" clip-path="url(#clip1)">
+                                <g id="Group">
+                                    <path id="Vector"
+                                          d="M38.1951 -7H-5C-6.10457 -7 -7 -6.10457 -7 -5V35C-7 36.1046 -6.10457 37 -5 37H38.1951C39.2997 37 40.1951 36.1046 40.1951 35V-5C40.1951 -6.10457 39.2997 -7 38.1951 -7Z"
+                                          fill="#5F5CFF"/>
+                                </g>
+                                <g id="Frame_2" clip-path="url(#clip2)">
+                                    <g id="GejSsI 2">
+                                        <g id="Group_2">
+                                            <path id="Vector_2"
+                                                  d="M8.88144 19.0638C7.38115 19.0507 6.23806 17.9496 6.26664 16.5733C6.29521 15.1969 7.46688 14.1745 9.01004 14.2007C10.4532 14.2269 11.6106 15.3149 11.5963 16.6519C11.582 18.0151 10.3817 19.09 8.88144 19.0638Z"
+                                                  fill="white"/>
+                                            <path id="Vector_3"
+                                                  d="M24.313 14.2012C25.7847 14.2012 26.9278 15.2629 26.9421 16.6131C26.9563 17.9894 25.7847 19.0643 24.2844 19.0643C22.7841 19.0643 21.5981 17.9763 21.6124 16.6131C21.6124 15.2498 22.7984 14.2012 24.313 14.2012Z"
+                                                  fill="white"/>
+                                            <path id="Vector_4"
+                                                  d="M32.3288 16.0757C32.286 15.6169 32.3146 15.1581 32.3146 14.6993C32.3146 14.542 32.3146 14.3978 32.3146 14.2405C32.3146 10.4785 30.2713 8.48608 26.1562 8.26324C25.6847 8.23703 25.6418 8.01419 25.5846 7.68649C24.7988 3.8065 21.0695 0.909611 16.7829 0.857178C12.382 0.804746 8.53838 3.57055 7.66678 7.50298C7.53818 8.09284 7.32386 8.26324 6.66658 8.26324C3.86602 8.26324 1.55128 10.0197 1.00831 12.5627C0.736828 13.808 0.951157 15.0926 0.894002 16.3509C0.879714 16.6786 1.10833 16.6786 1.36552 16.6786C2.07995 16.6786 2.79438 16.6524 3.50881 16.6917C3.99462 16.718 4.09464 16.5082 4.16608 16.1281C4.53759 13.7949 6.5237 12.1957 8.99562 12.2219C11.4533 12.2481 13.4251 13.8866 13.7394 16.2199C13.7966 16.6 13.9252 16.6917 14.2967 16.6786C15.8256 16.6655 17.3401 16.6655 18.869 16.6786C19.312 16.6786 19.4548 16.5738 19.512 16.1412C19.8835 13.4016 22.7698 11.6058 25.5846 12.3792C27.5565 12.9166 28.7424 14.175 29.0568 16.0626C29.1282 16.5082 29.2568 16.7311 29.7998 16.6786C30.4142 16.6262 31.0429 16.6393 31.6573 16.6786C32.1431 16.7048 32.386 16.5869 32.3288 16.0757ZM22.384 8.26324C20.4265 8.23703 18.4832 8.25014 16.5257 8.25014C14.6396 8.25014 12.7678 8.25014 10.8817 8.25014C10.0815 8.25014 10.0673 8.23703 10.3102 7.51608C11.1675 4.97312 13.7823 3.19042 16.6257 3.21663C19.512 3.24285 22.0554 5.03866 22.8984 7.58162C23.0699 8.09284 23.027 8.27635 22.384 8.26324Z"
+                                                  fill="white"/>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                    <defs>
+                        <clipPath id="clip0">
+                            <rect width="1440" height="3340" fill="white" transform="translate(-127 -30)"/>
+                        </clipPath>
+                        <clipPath id="clip1">
+                            <rect width="129" height="44" fill="white" transform="translate(-7 -7)"/>
+                        </clipPath>
+                        <clipPath id="clip2">
+                            <rect width="31.4634" height="26.7143" fill="white"
+                                  transform="translate(0.865723 0.857422)"/>
+                        </clipPath>
+                    </defs>
+                </svg>
+
+                    <svg viewBox="0 0 17 7" fill="none" xmlns="http://www.w3.org/2000/svg" className='line'>
+<path
+    d="M8.59721 6.57154C4.98221 6.41424 2.22451 4.95924 0.452731 1.99681C0.209825 1.59046 0.138383 1.3283 0.752791 1.15789C1.32433 1.00059 1.8673 0.554918 2.39598 0.594242C2.92465 0.633567 3.02467 1.38073 3.35331 1.78708C6.19673 5.2214 11.9979 4.88059 14.2841 1.10546C14.5984 0.594244 14.8556 0.450054 15.3986 0.764648C15.6415 0.895729 15.9272 0.974378 16.1844 1.06613C17.0846 1.40695 17.0989 1.42005 16.6131 2.18032C14.8556 4.99856 12.1551 6.40113 8.59721 6.57154Z"
+    fill="white"/>
+</svg>
+                </span>
+              <b className='deo'>DEO</b>
+              <i>
+                  <svg className='location' viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M0.5 8.31776C0.5 3.71789 4.34388 0 8.99345 0C13.6561 0 17.5 3.71789 17.5 8.31776C17.5 10.6357 16.657 12.7876 15.2695 14.6116C13.7388 16.6235 11.8522 18.3765 9.72854 19.7524C9.24251 20.0704 8.80387 20.0944 8.27045 19.7524C6.13474 18.3765 4.24809 16.6235 2.7305 14.6116C1.34198 12.7876 0.5 10.6357 0.5 8.31776ZM6.19423 8.57675C6.19423 10.1177 7.45166 11.3297 8.99345 11.3297C10.5362 11.3297 11.8058 10.1177 11.8058 8.57675C11.8058 7.0478 10.5362 5.77683 8.99345 5.77683C7.45166 5.77683 6.19423 7.0478 6.19423 8.57675Z"
+                            fill="#5F5CFF"/>
+                  </svg>
+              </i>
+              <a className='place' href='#'>Երևան</a>
+              <button type='button' className='addListButton'>Տեղադրել հայտարարություն</button>
+              <span className='border'>
+                </span>
+              <button type='button' className='chengLen'>Հայ</button>
+          </div>
+          <div id='filterField'>
+              <img className='passenger' src={car1}/>
+              <p className='car1'>Մարդատար</p>
+              <img className='MotorEquipment' src={car2}/>
+              <p className='car2'>Մոտոտեխնիկա</p>
+              <img className='Bus' src={car3}/>
+              <p className='car3'>Ավտոբուս</p>
+              <img className='Truck' src={car4}/>
+              <p className='car4'>Բեռնատար</p>
+              <img className='SpecialEquipment' src={car5}/>
+              <p className='car5'>Հատուկ տեխնիկա</p>
+              <img className='WaterEquipment' src={car6}/>
+              <p className='car6'>Ջրային տեխնիկա</p>
+          </div>
       </div>
-    </header>
+
   )
 }
 
